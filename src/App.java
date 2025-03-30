0502cc6cd5ef5b4066ce473283e023e6f1c5eb7c
isaidi28 <isaidines28@gmail.com>
2025-03-30 00:51:15 -0400

    Lancement Fenetre

--- src/App.java
+++ src/App.java
@@ -1,6 +1,28 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Puisque chaque carré est 32 pixel, faire 21*32 = longueur, et 19*32 = largeur
+       int nbLigne=21;
+       int nbColonne = 19;
+       int tailleTuile=32;
+       int largeurFenetre= nbColonne * tailleTuile;
+       int longueureFenetre = nbLigne * tailleTuile;
+
+       //Creation de la fenetre
+       JFrame fenetre  = new JFrame("Pac Man");
+       //rendre la fenetre visible
+       fenetre.setVisible(true);
+       //mettre la taille de la fenetre
+       fenetre.setSize(largeurFenetre, longueureFenetre);
+       //Ouvrir la fenetre dans un endroit visible et centré
+       fenetre.setLocationRelativeTo(null);
+       //ne pas laisser l'utilisateur reduire/agrandir la fenetre
+       fenetre.setResizable(false);
+       //Si l'utilisateur veut fermer la fenetre lorque il clique sur x
+       fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
-    //Test
+   
 }
